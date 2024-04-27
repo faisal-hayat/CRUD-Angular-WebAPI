da651de022b309c29a500b321f45e81c8f32b77a
Muhammad Faisal Hayat <faisal.hayat@routetrading.co.uk>
2024-04-27 18:22:54 +0500

    route has been added

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { NotFoundError } from 'rxjs';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: EmployeeListComponent
+    }, 
+    {
+        path: 'employee-list',
+        component: EmployeeListComponent
+    },
+    {
+        // wild card route 
+        path: "**",
+        component: NotFoundError
+    }
+];
