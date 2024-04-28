a2fa05b22068333b0a2b149833c40d8fe7444f60
Muhammad Faisal Hayat <faisal.hayat@routetrading.co.uk>
2024-04-28 18:25:00 +0500

    employee-form has been added

---
+++ front-end/src/app/components/employee-form/employee-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee-form.component.html',
+  styleUrl: './employee-form.component.css'
+})
+export class EmployeeFormComponent {
+
+}
--- front-end/src/app/components/employee-list/employee-list.component.ts
+++ front-end/src/app/components/employee-list/employee-list.component.ts
@@ -3,14 +3,17 @@ import { Employee } from '../../interfaces/employee';
 import { EmployeeServiceService } from '../../services/employee-service.service';
 import { HttpClientModule } from '@angular/common/http';
 import {MatTableModule} from '@angular/material/table';
-import { error } from 'console';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
   imports: [
     HttpClientModule, 
-    MatTableModule
+    MatTableModule,
+    MatButtonModule,
+    RouterModule
   ],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
