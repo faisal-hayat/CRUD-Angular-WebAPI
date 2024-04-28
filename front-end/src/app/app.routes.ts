92ab8f6c0b223207a568db1f0b701d7e93186e79
Muhammad Faisal Hayat <faisal.hayat@routetrading.co.uk>
2024-04-28 18:32:49 +0500

    route has been updated

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { NotFoundError } from 'rxjs';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
 
 export const routes: Routes = [
     {
@@ -10,6 +11,10 @@ export const routes: Routes = [
     {
         path: 'employee-list',
         component: EmployeeListComponent
+    },
+    {
+        path: 'create-employee',
+        component: EmployeeFormComponent
     },
     {
         // wild card route 
