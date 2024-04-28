099f36290ba0bf426fa43f9875032733fbbc7074
Muhammad Faisal Hayat <faisal.hayat@routetrading.co.uk>
2024-04-28 16:04:38 +0500

    getAllEmployees() endpoint has been added

--- front-end/src/app/components/employee-list/employee-list.component.ts
+++ front-end/src/app/components/employee-list/employee-list.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { Employee } from '../../interfaces/employee';
 import { EmployeeServiceService } from '../../services/employee-service.service';
-import { HttpClientModule, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import { error } from 'console';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
   imports: [
-    HttpClientModule
+    HttpClientModule, 
+    MatTableModule
   ],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
@@ -16,17 +18,30 @@ import { Observable } from 'rxjs';
 export class EmployeeListComponent {
   // define props here
   employees: Employee[] = [];
+  errorMessage: string = "";
+  displayedColumns : string[] = ["id", "name", "email", "phone", "age", "salary"];
 
   constructor(private employeeService: EmployeeServiceService){
     this.employeeService = employeeService;
+    
   }
   //#region: life cycle methods
   ngOnInit(){
     this.employeeService.getAllEmployees().subscribe((res: Employee[]) => {
       this.employees = res;
       console.log("employee list is : "+this.employees)
+    },
+    error => {
+      this.errorMessage = "An error occurred while fetching data";
     })
   }
   //#endregion
+}
 
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
 }
